96484d06f90c6af05392cdb1a3348c0dc293ef8d
MFathurrochim <faturohimskd06@gmail.com>
2024-09-05 17:08:03 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,45 +3,81 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
     }
 
+    public function luas() : float {
+        return self::PHI * $this->jari_jari * $this->jari_jari;
+    }
 
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling() : float {
+        return 2 * self::PHI * $this->jari_jari;
     }
 }
-
 
 class Bola
 {
     const PHI = 3.14;
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
+    public function volume() : float {
+        return (4 / 3) * self::PHI * pow($this->jari_jari, 3);
     }
 }
-
 
 class Tabung
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume() : float {
+        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
-
 
 class Kerucut
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume() : float {
+        return (1 / 3) * self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
 
-$nasi = new Kerucut();
-$nasi_tumpeng = $nasi->volume(4, 10);
-echo "Volume nasi tumpeng adalah {$nasi_tumpeng} \n";
+
+$lingkaran = new Lingkaran(7);
+echo "Luas lingkaran: " . $lingkaran->luas() . "\n";
+echo "Keliling lingkaran: " . $lingkaran->keliling() . "\n";
+
+// Contoh penggunaan class Bola
+$bola = new Bola(7);
+echo "Volume bola: " . $bola->volume() . "\n";
+
+$tabung = new Tabung(7, 10);
+echo "Volume tabung: " . $tabung->volume() . "\n";
+
+$kerucut = new Kerucut(7, 10);
+echo "Volume kerucut: " . $kerucut->volume() . "\n";
+
+?>
+
